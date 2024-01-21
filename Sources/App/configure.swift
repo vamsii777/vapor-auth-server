bd1c2fa3757f48e1de0ca577e7a2e1f1d13b1d02
Vamsi Madduluri <vamsi.madduluri@icloud.com>
2024-01-21 14:40:24 +0530

    Update server port and add migrations and middleware

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -7,15 +7,34 @@ import VaporOAuth
 // configures your application
 public func configure(_ app: Application) async throws {
     app.logger.logLevel = .notice
-    app.http.server.configuration.port = 8080
+    app.http.server.configuration.port = 8090
     try configureDatabases(app)
     app.databases.default(to: .main)
+    
+    app.migrations.add(CreateAccessToken(), to: .main)
+    app.migrations.add(CreateAuthorizationCode(), to: .main)
+    app.migrations.add(CreateRefreshToken(), to: .main)
+    app.migrations.add(CreateResourceServer(), to: .main)
+    app.migrations.add(CreateIDToken(), to: .main)
+    app.migrations.add(CreateClient(), to: .main)
+    app.migrations.add(CreateUser(), to: .main)
+    app.migrations.add(CreateCryptoKey(), to: .main)
+    app.migrations.add(CreateKeyOperationLog(), to: .main)
+    app.migrations.add(SeedClient(), to: .main)
+    app.migrations.add(SeedUser(), to: .main)
+    app.migrations.add(SeedResourceServer(), to: .main)
+    app.migrations.add(SeedPrivateCryptoKey(), to: .main)
+    app.migrations.add(SeedPublicCryptoKey(), to: .main)
+    
     try await app.autoMigrate().get()
     
     // Correctly create a CryptoKeysRepository instance by passing the database.
     let cryptoKeysRepository = CryptoKeysRepository(database: app.db)
     
     let keyManagementService = MyKeyManagementService(app: app, cryptoKeysRepository: cryptoKeysRepository)
+    
+    let sessionsMiddleware = app.sessions.middleware
+    app.middleware.use(sessionsMiddleware)
     
     app.lifecycle.use(
         OAuth2(
